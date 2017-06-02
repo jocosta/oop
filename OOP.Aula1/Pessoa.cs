192eac44295fbd2933280725c359f6fc883851b3
José Romualdo da Costa Filho <joseromualdo@outlook.com>
2017-06-02 00:07:39 -0300

    Add project files.

---
+++ OOP.Aula1/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Aula1
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+
+        public void Falar()
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
---
+++ OOP.Aula1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 19;
+            string nome = "Ovidio";
+
+            Pessoa aluno;
+            aluno = new Pessoa();
+
+            aluno.Nome = "Ovidio";
+            aluno.Idade = 20;
+
+            aluno.Falar();
+
+            Console.ReadKey();
+        }
+    }
+}
